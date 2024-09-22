59b240c00633ccfc70a92e672eaf6851024b5ea8
SteveG-tkorp <steve@tkorp.com>
2024-09-22 15:51:00 +0200

    navbar

---
+++ src/app/layouts/logo/Logo.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import logo from "@/assets/logo1.webp";
+import Image from "next/image";
+
+const Logo = () => {
+  return (
+    <Image
+      width={100}
+      //   height={75}
+      src={logo}
+      alt="Logo de MaCaveAVin"
+      style={{ borderRadius: "50%" }}
+    />
+  );
+};
+
+export default Logo;
---
+++ src/app/layouts/navbar.tsx
@@ -0,0 +1,61 @@
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarContent,
+  NavbarItem,
+  Link,
+  Button,
+} from "@nextui-org/react";
+import React from "react";
+import Logo from "./logo/Logo";
+
+const MainNavbar = () => {
+  return (
+    <Navbar>
+      <NavbarBrand>
+        <Logo />
+      </NavbarBrand>
+      <NavbarContent
+        className="hidden sm:flex gap-4"
+        justify="center">
+        <NavbarItem>
+          <Link
+            color="foreground"
+            href="#">
+            Mes vins
+          </Link>
+        </NavbarItem>
+        <NavbarItem isActive>
+          <Link
+            href="#"
+            aria-current="page">
+            Mes statistiques
+          </Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Link
+            color="foreground"
+            href="#">
+            Mon profil
+          </Link>
+        </NavbarItem>
+      </NavbarContent>
+      <NavbarContent justify="end">
+        <NavbarItem className="hidden lg:flex">
+          <Link href="#">Login</Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Button
+            as={Link}
+            color="primary"
+            href="#"
+            variant="flat">
+            Sign Up
+          </Button>
+        </NavbarItem>
+      </NavbarContent>
+    </Navbar>
+  );
+};
+
+export default MainNavbar;
